41687523af9a37b26befce0d3c82606520ce4e52
ajtayde001 <ajtayde001@gmail.com>
2024-04-22 16:47:29 +0530

    dating app

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  //Port
+  PORT: process.env.PORT || 5000,
+
+  //Secret key for API
+  SECRET_KEY: "(^Z2:1)8x#aUb.f",
+
+  //Gmail credentials for send email
+  EMAIL: "sudarshansonawane.sits.entc@gmail.com",
+  PASSWORD: "ssnt hbxd aaet hfmi",
+
+  //Secret key for jwt
+  JWT_SECRET: "kY4fai9sUT",
+
+  //baseURL
+  baseURL: "https://yourdomain.com/",
+
+  //Firebase server key for send notification through firebase
+  SERVER_KEY: "AAAA5Q43Nu0:APA91bEHrNxBFyLf8GoxAkwKb8Hv3BZmHSjbHQMlywU1Olv1u3oEt33FohkR4YpaG3zAGnX70XqqY9Xotu58hdQKE13xyD7fH0UmnHqh3Vsixta2DtecPnyj16ow5g2hA6pzp0nx_eG_",
+};
+npm
